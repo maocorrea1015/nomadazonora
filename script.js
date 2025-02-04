3a67709d3d8b244fc778d8c57bb333d6d0c67802
mauricio <mauriciocorrea.1015@gmail.com>
2025-02-04 12:39:51 -0500

    new landing

---
+++ script.js
@@ -0,0 +1,15 @@
+async function fetchRadioStats() {
+  try {
+    const response = await fetch(
+      "http://uk18freenew.listen2myradio.com:26530/stats?json=1"
+    );
+    const data = await response.json();
+    document.getElementById("song-title").innerText =
+      data.songtitle || "Desconocido";
+  } catch (error) {
+    console.error("Error obteniendo datos del stream:", error);
+  }
+}
+
+fetchRadioStats();
+setInterval(fetchRadioStats, 10000); // Actualiza cada 10 segundos
