c473bbb9c8a56e13bd5e7073d2f1449c1d94aab3
mauricio <mauriciocorrea.1015@gmail.com>
2025-02-05 00:47:55 -0500

    nuevo servidor

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-async function fetchRadioStats() {
-  try {
-    const response = await fetch(
-      "https://cors-anywhere.herokuapp.com/http://uk18freenew.listen2myradio.com:26530/stats?json=1"
-    );
-    const data = await response.json();
-    document.getElementById("song-title").innerText =
-      data.songtitle || "Desconocido";
-  } catch (error) {
-    console.error("Error obteniendo datos del stream:", error);
-  }
-}
-
-fetchRadioStats();
-setInterval(fetchRadioStats, 10000); // Actualiza cada 10 segundos
+document.getElementById("toggle-theme").addEventListener("click", function () {
+  document.body.classList.toggle("dark");
+});
